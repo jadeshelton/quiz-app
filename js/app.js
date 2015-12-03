0ea8159b2e38a21eb85cecc6b840d48ea02fe400
jadeshelton <jnshelton257@yahoo.com>
2015-12-02 21:37:55 -0800

    commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	/*$('#beer-cheers').hide().fadeIn(1000);*/4
+
+	$('#quiz').hide()
+	/*$('#intro').*/
+	$('#intro').show()
+	$('#outro').hide()
+
+	/*
+	$('#begin').one('click', function(){
+		$('#quiz').fadeIn(1000);
+		$('#intro').fadeOut(1000);
+	});
+	*/
+
+});
